3fc6e492c71743bb81a2315a7a237757dc0b0f14
Sakthieswaran <sakthieswaran.k@simplify3x.com>
2023-07-23 16:38:54 +0530

    Splash screen added

--- app/src/androidTest/java/imagetopdf/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/imagetopdf/example/myapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package imagetopdf.example.myapplication;
 
 import android.content.Context;
 
--- app/src/test/java/imagetopdf/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/imagetopdf/example/myapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package imagetopdf.example.myapplication;
 
 import org.junit.Test;
 
